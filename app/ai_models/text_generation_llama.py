26802e44d047cd31537e9d2e89f33e4119a02d8b
Anna Rechkalova <ann.rechkalova@gmail.com>
2025-01-29 20:52:54 +0200

    shortened prompt + changed translation model (works faster)

--- app/ai_models/text_generation_llama.py
+++ app/ai_models/text_generation_llama.py
@@ -10,10 +10,10 @@ def generate_answer(messages, answer=None):
         completion = client.chat.completions.create(
             model="meta-llama/Llama-3.2-3B-Instruct",
             messages=messages,
-            max_tokens=256,
+            max_tokens=512,
             temperature=0.7,
             top_p=0.9,
-            frequency_penalty=0.5
+            frequency_penalty=1
         )
         response = completion.choices[0].message["content"]
     else:
